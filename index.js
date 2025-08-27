436c27d7d78d8d8acf6f6aa3f2cde9ab8333220a
Forat Thalji <thaljiforat@gmail.com>
2025-08-27 19:30:17 +0300

    Fix backend for Render deployment

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ app.use(cors());
 // });
 
 
-const PORT = process.env.PORT || 9013; // 3000 للـ localhost
-server.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+const port = 9013;
+server.listen(port, () => {
+  console.log(`App running on port ${port}...`);
 });
